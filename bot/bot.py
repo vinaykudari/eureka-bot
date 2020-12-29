c347e0776c94ed2655b3e2bfc45fadff8294b3c7
Vinay Kudari <vinay@braveorbit.io>
2020-12-30 03:21:04 +0530

    Adjust parameters

--- bot/bot.py
+++ bot/bot.py
@@ -15,8 +15,8 @@ class EurekaBot(Controller):
         self,
         min_speed=10,
         max_speed=100,
-        turn_speed=30,
-        default_speed=30,
+        turn_speed=20,
+        default_speed=20,
         acceleration_duration=0.1,
     ):
         super().__init__(gpio_left_pin=15, gpio_right_pin=22)
@@ -41,25 +41,33 @@ class EurekaBot(Controller):
     def _accelerate(self, acceleration):
         # Set magnitude
         self.current_speed = self._get_speed(acceleration)
+        self.current_acceleration = acceleration
         self._set_speed(side='both', speed=self.current_speed)
-        time.sleep(self.acceleration_duration)
+        if acceleration > 0:
+            time.sleep(self.acceleration_duration)
 
-    def _move(self, acceleration, duration):
-        start_time = time.time()
-        while (time.time() - start_time <= duration) and self.current_speed < self.max_speed:
+    def move(self, acceleration, duration):
+        if duration > 0:
+            start_time = time.time()
+            while (time.time() - start_time <= duration) and self.current_speed < self.max_speed:
+                self._accelerate(acceleration=acceleration)
+        else:
             self._accelerate(acceleration=acceleration)
 
-    def _turn(self, duration, speed=0):
+        print(f'acc={self.current_acceleration}')
+
+    def turn(self, duration, speed=0):
         start_time = time.time()
         while time.time() - start_time <= duration:
             self._set_speed(side='both', speed=speed if speed else self.turn_speed)
 
-    def _break(self):
+    def break_bot(self):
         self._set_speed(side='both', speed=0)
+        self.current_acceleration = 0
 
     def _get_rotation_duration(self, speed, angle):
         duration = (self.rotation_constant * angle) / speed
-        print(f'duration = {duration}, angle={angle}')
+        # print(f'duration = {duration}, angle={angle}')
         return round(duration, 2)
 
     def _set_rotation_direction(self, direction):
@@ -70,15 +78,14 @@ class EurekaBot(Controller):
             self._set_pin_value(self.direction_pins['left'], Direction.backward.value)
             self._set_pin_value(self.direction_pins['right'], Direction.forward.value)
 
-    def _rotate(self, angle=0, duration=0, speed=0, direction='clockwise'):
+    def rotate(self, angle=0, duration=0, speed=0, direction='clockwise'):
         self._set_rotation_direction(direction=direction)
         if angle:
             duration = self._get_rotation_duration(
                 speed=speed if speed else self.turn_speed,
                 angle=angle,
             )
-        self._turn(duration=duration, speed=speed)
-        self._break()
+        self.turn(duration=duration, speed=speed)
 
     def stop(self):
         if self.current_acceleration > 0:
@@ -91,18 +98,18 @@ class EurekaBot(Controller):
         # Set direction
         self._set_pin_value(self.direction_pins.values(), Direction.forward.value)
         self.current_acceleration = acceleration
-        self._move(duration=duration, acceleration=acceleration)
+        self.move(duration=duration, acceleration=acceleration)
         self.stop()
 
     def move_backward(self, acceleration=0, duration=3):
         # Set direction
         self._set_pin_value(self.direction_pins.values(), Direction.backward.value)
         self.current_acceleration = acceleration
-        self._move(duration=duration, acceleration=acceleration)
+        self.move(duration=duration, acceleration=acceleration)
         self.stop()
 
     def move_left(self, speed=0, angle=90):
-        self._rotate(angle=angle, speed=speed, direction='anti-clockwise')
+        self.rotate(angle=angle, speed=speed, direction='anti-clockwise')
 
     def move_right(self, speed=0, angle=90):
-        self._rotate(angle=angle, speed=speed, direction='clockwise')
+        self.rotate(angle=angle, speed=speed, direction='clockwise')
---
+++ bot/sensors/proximity.py
@@ -0,0 +1,23 @@
+from apds9960 import APDS9960
+import smbus
+
+
+class Proximity:
+    def __init__(self, port, threshold=100):
+        self.port = port
+        self.threshold = threshold
+        self.bus = smbus.SMBus(port)
+
+        try:
+            self._initialize_sensor()
+        except Exception as ex:
+            raise ex
+
+    def _initialize_sensor(self):
+        self.apds = APDS9960(self.bus)
+        self.apds.setProximityIntLowThreshold(50)
+        self.apds.enableProximitySensor()
+
+    def get_proximity(self):
+        return self.apds.readProximity()
+
