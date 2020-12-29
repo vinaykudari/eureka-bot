3f108bb31c9fd47e0cfac2a39d03f3bc4833d609
Vinay Kudari <vinay@braveorbit.io>
2020-12-30 03:22:35 +0530

    Add Sensors And Movement

--- server/main.py
+++ server/main.py
@@ -1,11 +1,37 @@
+import asyncio
+import os
+import sys
+
 from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
 
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+from bot.sensors.proximity import Proximity
+from bot.bot import EurekaBot, Direction
+
 app = FastAPI()
+bot = EurekaBot()
+proximity_front = Proximity(port=0)
+proximity_back = Proximity(port=1)
 
 
 def resolve_coordinates(x, y):
-    print(x, y, y)
+    if y == 0.0:
+        bot.break_bot()
+
+    if ((300.0 <= x < 360.0) or (0.0 <= x < 60.0)) and (y != 0.0):
+        bot._set_pin_value(bot.direction_pins.values(), Direction.forward.value)
+        bot.move(acceleration=0, duration=0)
+    elif ((120.0 <= x < 180.0) or (180 <= x < 240.0)) and (y != 0.0):
+        bot._set_pin_value(bot.direction_pins.values(), Direction.backward.value)
+        bot.move(acceleration=0, duration=0)
+    elif (60.0 <= x < 120.0) and (y != 0.0):
+        bot.rotate(angle=5, direction='clockwise')
+    elif (240.0 <= x < 300) and (y != 0.0):
+        bot.rotate(angle=5, direction='anti-clockwise')
 
 
 @app.websocket("/ws")
@@ -14,8 +40,12 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_json()
-            x, y = round(data['x']), round(data['y'], 2)
-            await websocket.send_text(f"x: {x}, y: {y}")
+            resolve_coordinates(**data)
+            proximity = {
+                'proximity_front': proximity_front.get_proximity(),
+                'proximity_back': proximity_back.get_proximity()
+            }
+            await websocket.send_json(proximity)
+
     except WebSocketDisconnect:
         print('Client disconnected')
-
