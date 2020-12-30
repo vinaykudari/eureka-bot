ff518e0384f6db4696ca5f9b7e0c95154421aee8
Vinay Kudari <vinay@braveorbit.io>
2020-12-30 18:34:55 +0530

    Support Speed And Direction Control

--- server/main.py
+++ server/main.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 import sys
 
@@ -10,24 +9,30 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from bot.sensors.proximity import Proximity
-from bot.bot import EurekaBot, Direction
+from bot.bot import EurekaBot
 
 app = FastAPI()
 bot = EurekaBot()
-proximity_front = Proximity(port=0)
-proximity_back = Proximity(port=1)
+proximity_front = Proximity(port=0, threshold=100)
+proximity_back = Proximity(port=1, threshold=100)
 
 
-def resolve_coordinates(x, y):
+def act(x, y, turn_left, turn_right, speed):
+    if speed > 0:
+        bot.default_speed = speed
+
+    if turn_left is True:
+        bot.move_left()
+    elif turn_right is True:
+        bot.move_right()
+
     if y == 0.0:
         bot.break_bot()
 
     if ((300.0 <= x < 360.0) or (0.0 <= x < 60.0)) and (y != 0.0):
-        bot._set_pin_value(bot.direction_pins.values(), Direction.forward.value)
-        bot.move(acceleration=0, duration=0)
+        bot.move_forward(acceleration=0, duration=0)
     elif ((120.0 <= x < 180.0) or (180 <= x < 240.0)) and (y != 0.0):
-        bot._set_pin_value(bot.direction_pins.values(), Direction.backward.value)
-        bot.move(acceleration=0, duration=0)
+        bot.move_backward(acceleration=0, duration=0)
     elif (60.0 <= x < 120.0) and (y != 0.0):
         bot.rotate(angle=5, direction='clockwise')
     elif (240.0 <= x < 300) and (y != 0.0):
@@ -39,11 +44,11 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            data = await websocket.receive_json()
-            resolve_coordinates(**data)
+            request = await websocket.receive_json()
+            act(**request)
             proximity = {
-                'proximity_front': proximity_front.get_proximity(),
-                'proximity_back': proximity_back.get_proximity()
+                'front': proximity_front.get_proximity(),
+                'back': proximity_back.get_proximity()
             }
             await websocket.send_json(proximity)
 
