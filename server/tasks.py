28a920659bdeb8bd190fe5945235dba256fac410
Vinay Kudari <vinay@braveorbit.io>
2021-01-16 20:36:19 +0530

    Adjust Tasks According to Distance

--- server/tasks.py
+++ server/tasks.py
@@ -4,26 +4,38 @@ from bot.bot import EurekaBot, Direction
 
 
 class Tasks:
-    def __init__(self, bot: EurekaBot, front_proximity, rear_proximity, proximity_threshold):
+    def __init__(
+            self,
+            bot: EurekaBot,
+            tof,
+            proximity,
+            proximity_threshold,
+            distance_threshold,
+    ):
         self.bot = bot
-        self.front = front_proximity
-        self.rear = rear_proximity
+        self.tof = tof
+        self.proximity = proximity
         self.proximity_threshold = proximity_threshold
+        self.distance_threshold = distance_threshold
         self.can_move_forward = True
         self.can_move_backward = True
 
     def _can_move_forward(self):
-        self.can_move_forward = not (self.front.proximity > self.proximity_threshold)
+        self.can_move_forward = (self.tof.distance > self.distance_threshold)
         return self.can_move_forward
 
     def _can_move_backward(self):
-        self.can_move_backward = not (self.rear.proximity > self.proximity_threshold)
+        self.can_move_backward = not (self.proximity.proximity > self.proximity_threshold)
         return self.can_move_backward
+
+    async def get_distance(self):
+        while True:
+            self.tof.get_distance()
+            await asyncio.sleep(self.tof.timing)
 
     async def get_proximity(self):
         while True:
-            self.front.get_proximity()
-            self.rear.get_proximity()
+            self.proximity.get_proximity()
             await asyncio.sleep(0.1)
 
     async def avoid_collision(self):
